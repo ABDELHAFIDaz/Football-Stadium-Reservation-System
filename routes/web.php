532e7926d763ec4198ecb81c1ebcbc8b8c108063
ABD ELHAFID AZLIGUE <abdelhafidazligue162@gmail.com>
2026-05-02 00:58:53 +0100

    grouped routes with middlware or prefix, also formated the unformated files

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,22 @@ Route::get('/', function () {
     return view('home', compact('cities'));
 })->name('home');
 
-Route::get('/userDashboard', [UserDashboardController::class, 'index'])->name('user.dashboard')->middleware(['role:customer']);
-Route::patch('/updateProfile', [UserDashboardController::class, 'update'])->name('update.profile')->middleware(['role:customer']);
-
-Route::prefix('manager')->group(function () {
-    Route::get('/addStad', [StadiumController::class, 'addStaduim'])->name('manager.addStad');
-    Route::get('/editStad/{stadium}', [StadiumController::class, 'editStaduim'])->name('manager.editStad');
-    Route::get('/removeStad', [StadiumController::class, 'removeStadium'])->name('manager.removeStad');
-})->middleware(['role:manager']);
-
 Route::get('/pitches', [StadiumController::class, 'index'])->name('pitches');
 
 
+Route::middleware(['role:customer'])->group(function () {
+
+    Route::get('/userDashboard', [UserDashboardController::class, 'index'])->name('user.dashboard')->middleware(['role:customer']);
+    Route::patch('/updateProfile', [UserDashboardController::class, 'update'])->name('update.profile')->middleware(['role:customer']);
+});
+
+
+Route::prefix('manager')->group(function () {
+
+    Route::get('/addStad', [StadiumController::class, 'addStaduim'])->name('manager.addStad');
+    Route::get('/editStad/{stadium}', [StadiumController::class, 'editStaduim'])->name('manager.editStad');
+    Route::get('/removeStad', [StadiumController::class, 'removeStadium'])->name('manager.removeStad');
+})->middleware(['role:manager, admin']);
 
 
 
